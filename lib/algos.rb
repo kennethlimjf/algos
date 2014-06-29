d32301575362aa18848dd94136d3eacdcebdc90a
Kenneth Lim <kennethlimjf@gmail.com>
2014-06-30 06:23:47 +0800

    Included edge case for merge sort.

--- lib/algos.rb
+++ lib/algos.rb
@@ -4,15 +4,13 @@ module Algos
   module Sort
     class MergeSort
       def self.sort(x)
-        (x.size == 1) ? x : merge(sort(x[0, x.size/2]), sort(x[x.size/2, x.size]))
+        (x.size <= 1) ? x : merge(sort(x[0, x.size/2]), sort(x[x.size/2, x.size]))
       end
 
       private
         def self.merge(a, b)
           c = Array.new(a.size + b.size)
-          c.each_index do |i|
-            (a.first && b.first) ? (c[i] = (a.first < b.first) ? a.shift : b.shift) : (return c.compact! + ((b.size == 0) ? a : b))
-          end
+          c.each_index { |i| (a.first && b.first) ? (c[i] = (a.first < b.first) ? a.shift : b.shift) : (return c.compact! + ((b.size == 0) ? a : b)) }
         end
     end
   end
--- spec/algos_spec.rb
+++ spec/algos_spec.rb
@@ -20,6 +20,18 @@ describe Algos::Sort::MergeSort do
       sorted_ary = Algos::Sort::MergeSort.sort(unsorted_ary)
       expect(sorted_ary).to eq([1,2,3,4,5,6,7,8,8])
     end
+
+    it 'sorts alphabets' do
+      unsorted_ary = ['z','e','a','y','w','o']
+      sorted_ary = Algos::Sort::MergeSort.sort(unsorted_ary)
+      expect(sorted_ary).to eq(['a','e','o','w','y','z'])
+    end
+
+    it 'returns an empty array given an empty array' do
+      unsorted_ary = []
+      sorted_ary = Algos::Sort::MergeSort.sort(unsorted_ary)
+      expect(sorted_ary).to eq([])
+    end
   end
 
 end
